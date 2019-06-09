6517d1119b4c79d5f236c4c20cb651079e51850f
Bennett Marsh <bmarsh@physics.ucsb.edu>
2019-06-08 17:49:42 -0700

    reorganization

---
+++ pitchdf/GameJSONParser.py
@@ -0,0 +1,278 @@
+import os,sys
+import gzip, json, glob
+import datetime as dt
+from Output import Output
+from OutputROOT import OutputROOT
+from GameState import GameState
+
+class GameJSONParser:
+    ignore_actions = ("Passed Ball", "Wild Pitch", "Caught Stealing 2B",
+                      "Caught Stealing 3B", "Offensive Substitution", "Defensive Switch", 
+                      "Game Advisory","Stolen Base 2B", "Stolen Base 3B", "Defensive Sub",
+                      "Injury", "Runner Out", "Pickoff Error 1B", "Balk", "Pitch Challenge",
+                      "Pickoff 1B", "Ejection", "Defensive Indiff", "Stolen Base Home",
+                      "Pickoff Error 2B", "Other Advance", "Pickoff Caught Stealing 2B",
+                      "Pickoff 2B", "Caught Stealing Home","Error", "Pickoff Caught Stealing Home",
+                      "Pickoff 3B", "Pickoff Caught Stealing 3B")
+
+    def __init__(self, outputter):
+        self.game_state = GameState()
+        if not isinstance(outputter, Output):
+            raise TypeError("Must provide an Output object to the parser!")
+        self.output = outputter
+
+    def process_runner(self, runner):
+        pid = runner["details"]["runner"]["id"]
+        start = runner["movement"]["start"]
+        end = runner["movement"]["end"]
+        if end not in ["1B","2B","3B","4B","score",None]:
+            raise Exception("ERROR unknown runner end state: "+end)
+        isScore = (end == "score")
+        isOut = runner["movement"]["isOut"]
+        
+        base_map = {'1B':'first', '2B':'second', '3B':'third'}
+
+        if start not in [None,"4B"] and getattr(self.game_state, base_map[start]) == pid:
+            setattr(self.game_state,base_map[start], -1)
+        if end is not None and end not in ["score","4B"]:
+            setattr(self.game_state,base_map[end], pid)
+
+        if isScore and self.game_state.home_score > -1:
+            if self.game_state.half == "top":
+                self.game_state.away_score += 1
+            if self.game_state.half == "bottom":
+                self.game_state.home_score += 1
+    
+        self.game_state.base_state = 0
+        if self.game_state.first != -1:
+            self.game_state.base_state |= (1<<0)
+        if self.game_state.second != -1:
+            self.game_state.base_state |= (1<<1)
+        if self.game_state.third != -1:
+            self.game_state.base_state |= (1<<2)
+
+        if isOut:
+            self.game_state.o += 1
+    
+
+
+    def process_pitch(self, pitch):
+        class PitchInfo:
+            pass
+        pinfo = PitchInfo()
+
+        pinfo.des = pitch["details"]["description"]
+        pinfo.type = pitch["details"]["call"]["code"]
+        if "type" in pitch["details"]:
+            pinfo.pitch_type = pitch["details"]["type"]["code"]
+        elif "Automatic Ball" in pinfo.des:
+            pinfo.pitch_type = "IN"
+        else:
+            pinfo.pitch_type = "UN"
+
+        pinfo.x = pitch["pitchData"]["coordinates"].get("x",-9999)
+        pinfo.y = pitch["pitchData"]["coordinates"].get("y",-9999)
+        pinfo.px = pitch["pitchData"]["coordinates"].get("pX",-9999)
+        pinfo.pz = pitch["pitchData"]["coordinates"].get("pZ",-9999)
+        pinfo.pfx_x = pitch["pitchData"]["coordinates"].get("pfxX",-9999)
+        pinfo.pfx_z = pitch["pitchData"]["coordinates"].get("pfxZ",-9999)
+        pinfo.x0 = pitch["pitchData"]["coordinates"].get("x0",-9999)
+        pinfo.y0 = pitch["pitchData"]["coordinates"].get("y0",-9999)
+        pinfo.z0 = pitch["pitchData"]["coordinates"].get("z0",-9999)
+        pinfo.vx0 = pitch["pitchData"]["coordinates"].get("vX0",-9999)
+        pinfo.vy0 = pitch["pitchData"]["coordinates"].get("vY0",-9999)
+        pinfo.vz0 = pitch["pitchData"]["coordinates"].get("vZ0",-9999)
+        pinfo.ax = pitch["pitchData"]["coordinates"].get("aX",-9999)
+        pinfo.ay = pitch["pitchData"]["coordinates"].get("aY",-9999)
+        pinfo.az = pitch["pitchData"]["coordinates"].get("aZ",-9999)
+
+        pinfo.break_y = pitch["pitchData"]["breaks"].get("breakY",-9999)
+        pinfo.break_angle = pitch["pitchData"]["breaks"].get("breakAngle",-9999)
+        # for some reason the break angle is abs() valued in the JSON... correct based on sign of ax
+        if pinfo.ax > 0 and pinfo.break_angle > -9998:
+            pinfo.break_angle *= -1
+        pinfo.break_length = pitch["pitchData"]["breaks"].get("breakLength",-9999)
+        pinfo.spin_dir = pitch["pitchData"]["breaks"].get("spinDirection",-9999)
+        pinfo.spin_rate = pitch["pitchData"]["breaks"].get("spinRate",-9999)
+
+        pinfo.start_speed = pitch["pitchData"].get("startSpeed",-9999)
+        pinfo.end_speed = pitch["pitchData"].get("endSpeed",-9999)
+        pinfo.sz_top = pitch["pitchData"].get("strikeZoneTop",-9999)
+        pinfo.sz_bot = pitch["pitchData"].get("strikeZoneBottom",-9999)
+        pinfo.zone = pitch["pitchData"].get("zone",-9999)
+        pinfo.type_confidence = pitch["pitchData"].get("typeConfidence",-9999)
+
+        if "hitData" in pitch:
+            pinfo.hit_x = pitch["hitData"]["coordinates"].get("coordX", -9999)
+            pinfo.hit_y = pitch["hitData"]["coordinates"].get("coordY", -9999)
+            pinfo.hit_launchAngle = pitch["hitData"].get("launchAngle", -9999)
+            pinfo.hit_launchSpeed = pitch["hitData"].get("launchSpeed", -9999)
+            pinfo.hit_totalDistance = pitch["hitData"].get("totalDistance", -9999)
+            pinfo.hit_location = pitch["hitData"].get("location", -9999)
+            pinfo.hit_trajectory = pitch["hitData"].get("trajectory", -9999)
+
+
+        # print "Inning: {0}, Pitcher: {1}, Batter: {2}, Count: {3}-{4}, Outs: {5}, Pitch Type: {6}, Result: {7}".format(
+        #     self.game_state.inning"], self.game_state['pitcher'], self.game_state['batter'], self.game_state['b'], 
+        #     self.game_state['s'], self.game_state['o'], pinfo.pitch_type"], pinfo.type"])
+
+
+        self.output.add_entry(self.game_state, pinfo)
+
+        self.game_state.pitch_counts[self.game_state.pitcher] += 1
+        self.game_state.cur_pc = self.game_state.pitch_counts[self.game_state.pitcher]
+
+        if pinfo.type == "B":
+            self.game_state.b += 1
+        if pinfo.type == "S" and (self.game_state.s < 2 or pinfo.des != "Foul"):
+            self.game_state.s += 1
+
+
+        if pinfo.des != "Hit By Pitch": 
+            # sometimes the ball count is broken after a hit by pitch... 
+            # shouldn't matter anyway as it's the last pitch of AB
+            if self.game_state.b != pitch["count"]["balls"]:
+                raise Exception("Balls mismatch!")
+            if self.game_state.s != pitch["count"]["strikes"]:
+                raise Exception("Strikes mismatch!")
+
+
+    def process_atbat(self, atbat):
+            self.game_state.inning = atbat["about"]["inning"]
+            # if the half-inning changes, reset the runners and outs
+            if self.game_state.half != atbat["about"]["halfInning"]:
+                self.game_state.half = atbat["about"]["halfInning"]
+                self.game_state.first = -1
+                self.game_state.second = -1
+                self.game_state.third = -1
+                self.game_state.base_state = 0
+                self.game_state.o = 0
+
+            self.game_state.b = 0
+            self.game_state.s = 0
+            self.game_state.batter = atbat["matchup"]["batter"]["id"]
+            self.game_state.pitcher= atbat["matchup"]["pitcher"]["id"]
+            self.game_state.BH = atbat["matchup"]["batSide"]["code"]
+            self.game_state.PH = atbat["matchup"]["pitchHand"]["code"]
+
+            self.game_state.home_score_after = atbat["result"]["homeScore"]
+            self.game_state.away_score_after = atbat["result"]["awayScore"]
+
+            # get event type, check if it's the first time we've seen it
+            self.game_state.event = atbat["result"]["event"]
+            if self.game_state.event not in unique_events:
+                print "{0:25s}:{1}".format(self.game_state.event, self.game_state.gid)
+                unique_events.append(self.game_state.event)
+
+            if self.game_state.pitcher not in self.game_state.pitch_counts:
+                self.game_state.pitch_counts[self.game_state.pitcher] = 0
+
+            # each at-bat consists of a series of "events". Each event can be a "pitch" or an
+            # "action" (e.g. stolen base).
+            # "runners" can be concurrent with each event. Store as [event, [runners1,...]]
+            events = []
+            for event in atbat["playEvents"]:
+                events.append([event, []])
+            # for the "after at-bat", there can be runners but no official "event"
+            events.append([None, []])
+            # now insert the runner events at the appropriate spot
+            for runner in atbat["runners"]:
+                idx = runner["details"]["playIndex"]
+                events[idx][1].append(runner)
+
+            for event,runners in events:
+                if event is not None:
+                    if event["type"] == "pitch":
+                        self.process_pitch(event)
+                        pass
+                    elif event["type"] == "action":
+                        evttype = event["details"]["event"]
+                        if evttype == "Pitching Substitution":
+                            self.game_state.pitcher = event["player"]["id"]
+                            if self.game_state.pitcher not in self.game_state.pitch_counts:
+                                self.game_state.pitch_counts[self.game_state.pitcher] = 0
+                        elif evttype == "Umpire Substitution":
+                            des = event["details"]["description"]
+                            if "HP" in des or "home plate" in des.lower():
+                                self.game_state.umpire = event["umpire"]["id"]
+                        elif evttype == "Pitcher Switch":
+                            if "left-handed" in event["details"]["description"]:
+                                self.game_state.PH = "L"
+                            if "right-handed" in event["details"]["description"]:
+                                self.game_state.PH = "R"
+                        elif evttype in self.ignore_actions:
+                            pass
+                        else:
+                            raise Exception("Unknown action: "+evttype)
+                    elif event["type"] not in ["pickoff"]:
+                        raise Exception("Unknown event type "+event["type"])
+
+                for runner in runners:
+                    self.process_runner(runner)
+
+            if self.game_state.home_score_after != self.game_state.home_score:
+                print self.game_state
+                raise Exception("Home score mismatch!")
+            if self.game_state.away_score_after != self.game_state.away_score:
+                print self.game_state
+                raise Exception("Away score mismatch!")
+
+            self.game_state.o = atbat["count"]["outs"]
+
+
+    def parse_game(self, gd):
+        g = gd["gameData"]
+        print g["game"]["id"], g["game"]["pk"], g["datetime"]["dateTime"]
+
+        self.game_state = GameState()
+
+        self.game_state.gid = g["game"]["id"]
+        self.gamePk = g["game"]["pk"]
+        date = dt.datetime.strptime(g["datetime"]["dateTime"].split("T")[0],"%Y-%m-%d")
+        hour = int(g["datetime"]["time"].split(":")[0])
+        if g["datetime"]["ampm"] == "PM" and hour != 12:
+            hour += 12
+        minute = int(g["datetime"]["time"].split(":")[1])
+        self.game_state.date = dt.datetime(date.year, date.month, date.day, hour, minute)
+        self.game_state.away_team = g["teams"]["away"]["teamCode"]
+        self.game_state.home_team = g["teams"]["away"]["teamCode"]
+        self.game_state.DH = int(g["game"]["id"].split("-")[-1])
+
+        ld = gd["liveData"]
+        umps = ld["boxscore"]["officials"]
+        for ump in umps:
+            if "home" in ump["officialType"].lower():
+                self.game_state.umpire = ump["official"]["id"]
+
+        for play in ld["plays"]["allPlays"]:
+            if play["result"]["type"] == "atBat":
+                self.process_atbat(play)
+            else:
+                raise Exception("Unknown play type "+events["result"]["type"])
+
+
+if __name__=="__main__":
+
+    year = 2018
+    gids = sorted([x.split("/")[-1] for x in glob.glob("/nfs-7/userdata/bemarsh/gamelogs/{0}/gid*".format(year))])
+    # gids = ["gid_2018_04_18_chamlb_oakmlb_1"]
+
+    output = OutputROOT("pitches_{0}_fromJSON.root".format(year))
+    parser = GameJSONParser(output)
+
+    unique_events = []                
+
+    indir = "/nfs-7/userdata/bemarsh/gamelogs/{0}".format(year)
+    for gid in gids:
+        fname = os.path.join(indir,gid,"livefeed.json.gz")
+        if not os.path.exists(fname):
+            print "ERROR: gid {0} does not exist. Skipping.".format(gid)
+            continue
+
+        gd = None
+        with gzip.open(fname, "rb") as fid:
+            gd = json.loads(fid.read().decode("utf-8"))
+
+        parser.parse_game(gd)
+
+    output.write()
---
+++ pitchdf/OutputROOT.py
@@ -0,0 +1,282 @@
+import numpy as np
+import ROOT
+from Output import Output
+
+class OutputROOT(Output):
+    def __init__(self, output_file):
+        self._fout = ROOT.TFile(output_file, "RECREATE")
+        self._t = ROOT.TTree("Pitches","Pitches")
+
+        self._unique_pitch_types = [
+            'UN', 'SI', 'SL', 'FF', 'FC', 'CU', 'CH', 'FT', 'FS', 'KC', 
+            'EP', 'FO', 'PO', 'SC', 'KN', 'AB', 'IN', 'FA'
+            ]
+
+        ## game state stuff
+        self._year = np.zeros(1, dtype=int)
+        self._month = np.zeros(1, dtype=int)
+        self._day = np.zeros(1, dtype=int)
+        self._hour = np.zeros(1, dtype=int)
+        self._minute = np.zeros(1, dtype=int)
+        self._DH = np.zeros(1, dtype=int)
+        self._inning  = np.zeros(1, dtype=int)
+        self._batter  = np.zeros(1, dtype=int)
+        self._pitcher = np.zeros(1, dtype=int)
+        self._balls   = np.zeros(1, dtype=int)
+        self._strikes = np.zeros(1, dtype=int)
+        self._outs    = np.zeros(1, dtype=int)
+        self._pitch_count = np.zeros(1, dtype=int)
+        self._home_score = np.zeros(1, dtype=int)
+        self._away_score = np.zeros(1, dtype=int)
+        self._home_score_after = np.zeros(1, dtype=int)
+        self._away_score_after = np.zeros(1, dtype=int)
+        self._runner_first = np.zeros(1, dtype=int)
+        self._runner_second = np.zeros(1, dtype=int)
+        self._runner_third = np.zeros(1, dtype=int)
+        self._base_state = np.zeros(1, dtype=int)
+        self._umpire = np.zeros(1, dtype=int)
+        self._is_last_pitch = np.zeros(1,dtype=int)
+        self._gamePk = np.zeros(1,dtype=int)
+
+        self._gid = ROOT.std.string()
+        self._away_team = ROOT.std.string()
+        self._home_team = ROOT.std.string()
+        self._half = ROOT.std.string()
+        self._batter_hand = ROOT.std.string()
+        self._pitcher_hand = ROOT.std.string()
+        self._event = ROOT.std.string()
+
+        ## pitchf/x data
+        self._des = ROOT.std.string()
+        self._type = ROOT.std.string()
+        self._pitch_type = ROOT.std.string()
+        self._strike_type = ROOT.std.string()  ## C,S,F,FT,FB,MB,B for called, swinging, foul, foul tip, foul bunt, missed bunt, ball
+
+        self._x = np.zeros(1, dtype=float)
+        self._y = np.zeros(1, dtype=float)
+        self._start_speed = np.zeros(1, dtype=float)
+        self._end_speed = np.zeros(1, dtype=float)
+        self._sz_top = np.zeros(1, dtype=float)
+        self._sz_bot = np.zeros(1, dtype=float)
+        self._pfx_x = np.zeros(1, dtype=float)
+        self._pfx_z = np.zeros(1, dtype=float)
+        self._px = np.zeros(1, dtype=float)
+        self._pz = np.zeros(1, dtype=float)
+        self._x0 = np.zeros(1, dtype=float)
+        self._y0 = np.zeros(1, dtype=float)
+        self._z0 = np.zeros(1, dtype=float)
+        self._vx0 = np.zeros(1, dtype=float)
+        self._vy0 = np.zeros(1, dtype=float)
+        self._vz0 = np.zeros(1, dtype=float)
+        self._ax = np.zeros(1, dtype=float)
+        self._ay = np.zeros(1, dtype=float)
+        self._az = np.zeros(1, dtype=float)
+        self._break_y = np.zeros(1, dtype=float)
+        self._break_angle = np.zeros(1, dtype=float)
+        self._break_length = np.zeros(1, dtype=float)
+        self._type_confidence = np.zeros(1, dtype=float)
+        self._zone = np.zeros(1, dtype=int)
+        self._nasty = np.zeros(1, dtype=int)
+        self._spin_dir = np.zeros(1, dtype=float)
+        self._spin_rate = np.zeros(1, dtype=float)
+        # self._pitch_type_id = np.zeros(1, dtype=int)
+
+        # statcast hit data
+        self._hit_x = np.zeros(1, dtype=float)
+        self._hit_y = np.zeros(1, dtype=float)
+        self._hit_launchAngle = np.zeros(1, dtype=float)
+        self._hit_launchSpeed = np.zeros(1, dtype=float)
+        self._hit_totalDistance = np.zeros(1, dtype=float)
+        self._hit_location = np.zeros(1, dtype=int)
+        self._hit_trajectory = ROOT.std.string()
+        self._hit_hardness = ROOT.std.string()
+
+        # game state stuff
+        self._t.Branch("year", self._year, 'year/I')
+        self._t.Branch("month", self._month, 'month/I')
+        self._t.Branch("day", self._day, 'day/I')
+        self._t.Branch("hour", self._hour, 'hour/I')
+        self._t.Branch("minute", self._minute, 'minute/I')
+        self._t.Branch("DH", self._DH, 'DH/I')
+        self._t.Branch("inning", self._inning, 'inning/I')
+        self._t.Branch("batter", self._batter, 'batter/I')
+        self._t.Branch("pitcher", self._pitcher, 'pitcher/I')
+        self._t.Branch("balls", self._balls, 'balls/I')
+        self._t.Branch("strikes", self._strikes, 'strikes/I')
+        self._t.Branch("outs", self._outs, 'outs/I')
+        self._t.Branch("pitch_count", self._pitch_count, 'pitch_count/I')
+        self._t.Branch("home_score", self._home_score, 'home_score/I')
+        self._t.Branch("away_score", self._away_score, 'away_score/I')
+        self._t.Branch("home_score_after", self._home_score_after, 'home_score_after/I')
+        self._t.Branch("away_score_after", self._away_score_after, 'away_score_after/I')
+        self._t.Branch("runner_first", self._runner_first, 'runner_first/I')
+        self._t.Branch("runner_second", self._runner_second, 'runner_second/I')
+        self._t.Branch("runner_third", self._runner_third, 'runner_third/I')
+        self._t.Branch("base_state", self._base_state, 'base_state/I')
+        self._t.Branch("umpire", self._umpire, 'umpire/I')
+        self._t.Branch("is_last_pitch", self._is_last_pitch, 'is_last_pitch/I')
+        self._t.Branch("gamePk", self._gamePk, "gamePk/I")
+        
+        self._t.Branch("gid", self._gid)
+        self._t.Branch("away_team", self._away_team)
+        self._t.Branch("home_team", self._home_team)
+        self._t.Branch("half", self._half)
+        self._t.Branch("batter_hand", self._batter_hand)
+        self._t.Branch("pitcher_hand", self._pitcher_hand)
+        self._t.Branch("event", self._event)
+        
+        # pitchf/x data
+        self._t.Branch("des", self._des)
+        self._t.Branch("type", self._type)
+        self._t.Branch("pitch_type", self._pitch_type)
+        self._t.Branch("strike_type", self._strike_type)
+
+        self._t.Branch("x", self._x, 'x/D')
+        self._t.Branch("y", self._y, 'y/D')
+        self._t.Branch("start_speed", self._start_speed, 'start_speed/D')
+        self._t.Branch("end_speed", self._end_speed, 'end_speed/D')
+        self._t.Branch("sz_top", self._sz_top, 'sz_top/D')
+        self._t.Branch("sz_bot", self._sz_bot, 'sz_bot/D')
+        self._t.Branch("pfx_x", self._pfx_x, 'pfx_x/D')
+        self._t.Branch("pfx_z", self._pfx_z, 'pfx_z/D')
+        self._t.Branch("px", self._px, 'px/D')
+        self._t.Branch("pz", self._pz, 'pz/D')
+        self._t.Branch("x0", self._x0, 'x0/D')
+        self._t.Branch("y0", self._y0, 'y0/D')
+        self._t.Branch("z0", self._z0, 'z0/D')
+        self._t.Branch("vx0", self._vx0, 'vx0/D')
+        self._t.Branch("vy0", self._vy0, 'vy0/D')
+        self._t.Branch("vz0", self._vz0, 'vz0/D')
+        self._t.Branch("ax", self._ax, 'ax/D')
+        self._t.Branch("ay", self._ay, 'ay/D')
+        self._t.Branch("az", self._az, 'az/D')
+        self._t.Branch("break_y", self._break_y, 'break_y/D')
+        self._t.Branch("break_angle", self._break_angle, 'break_angle/D')
+        self._t.Branch("break_length", self._break_length, 'break_length/D')
+        self._t.Branch("type_confidence", self._type_confidence, 'type_confidence/D')
+        self._t.Branch("zone", self._zone, 'zone/I')
+        self._t.Branch("nasty", self._nasty, 'nasty/I')
+        self._t.Branch("spin_dir", self._spin_dir, 'spin_dir/D')
+        self._t.Branch("spin_rate", self._spin_rate, 'spin_rate/D')
+        # self._t.Branch("pitch_type_id", self._pitch_type_id, 'pitch_type_id/I')
+        self._t.Branch("hit_x", self._hit_x, "hit_x/D")
+        self._t.Branch("hit_y", self._hit_y, "hit_y/D")
+        self._t.Branch("hit_launchAngle", self._hit_launchAngle, "hit_launchAngle/D")
+        self._t.Branch("hit_launchSpeed", self._hit_launchSpeed, "hit_launchSpeed/D")
+        self._t.Branch("hit_totalDistance", self._hit_totalDistance, "hit_totalDistance/D")
+        self._t.Branch("hit_location", self._hit_location, "hit_location/I")
+        self._t.Branch("hit_trajectory", self._hit_trajectory)
+        
+    def add_entry(self, game_state, pitch):
+        # game state
+        self._year[0] = game_state.date.year
+        self._month[0] = game_state.date.month
+        self._day[0] = game_state.date.day
+        self._hour[0] = game_state.date.hour
+        self._minute[0] = game_state.date.minute
+        self._DH[0] = game_state.DH
+        self._inning[0] = game_state.inning
+        self._batter[0] = game_state.batter
+        self._pitcher[0] = game_state.pitcher
+        self._balls[0] = game_state.b
+        self._strikes[0] = game_state.s
+        self._outs[0] = game_state.o
+        self._pitch_count[0] = game_state.cur_pc
+        self._home_score[0] = game_state.home_score
+        self._away_score[0] = game_state.away_score
+        self._home_score_after[0] = game_state.home_score_after
+        self._away_score_after[0] = game_state.away_score_after
+        self._runner_first[0] = game_state.first
+        self._runner_second[0] = game_state.second
+        self._runner_third[0] = game_state.third
+        self._base_state[0] = game_state.base_state
+        self._umpire[0] = game_state.umpire
+        self._gamePk[0] = game_state.gamePk
+        
+        n = ROOT.std.string.npos
+        self._gid.replace(0, n, game_state.gid)
+        self._away_team.replace(0, n, game_state.away_team)
+        self._home_team.replace(0, n, game_state.home_team)
+        self._half.replace(0, n, game_state.half)
+        self._batter_hand.replace(0, n, game_state.BH)
+        self._pitcher_hand.replace(0, n, game_state.PH)
+        self._event.replace(0, n, game_state.event)
+        
+        #pitchf/x data
+        self._des.replace(0, n, pitch.des)
+        self._type.replace(0, n, pitch.type)
+        self._pitch_type.replace(0, n, pitch.pitch_type)
+        st = pitch.type
+        if st=='S':
+            if 'Called' in pitch.des:
+                st = 'C'
+            elif 'Swinging' in pitch.des:
+                st = 'S'
+            elif 'Foul Tip' in pitch.des:
+                st = 'FT'
+            elif 'Foul Bunt' in pitch.des:
+                st = 'FB'
+            elif 'Foul' in pitch.des:
+                st = 'F'
+            elif 'Missed Bunt' in pitch.des:
+                st = 'MB'
+            else:
+                raise Exception("Unknown strike description: "+pitch.des)
+        self._strike_type.replace(0, n, st)
+
+        self._x[0] = float(getattr(pitch,"x",-9999))
+        self._y[0] = float(getattr(pitch,"y",-9999))
+        self._start_speed[0] = float(getattr(pitch,"start_speed",-9999))
+        self._end_speed[0] = float(getattr(pitch,"end_speed",-9999))
+        self._sz_top[0] = float(getattr(pitch,"sz_top",-9999))
+        self._sz_bot[0] = float(getattr(pitch,"sz_bot",-9999))
+        self._pfx_x[0] = float(getattr(pitch,"pfx_x",-9999))
+        self._pfx_z[0] = float(getattr(pitch,"pfx_z",-9999))
+        self._px[0] = float(getattr(pitch,"px",-9999))
+        self._pz[0] = float(getattr(pitch,"pz",-9999))
+        self._x0[0] = float(getattr(pitch,"x0",-9999))
+        self._y0[0] = float(getattr(pitch,"y0",-9999))
+        self._z0[0] = float(getattr(pitch,"z0",-9999))
+        self._vx0[0] = float(getattr(pitch,"vx0",-9999))
+        self._vy0[0] = float(getattr(pitch,"vy0",-9999))
+        self._vz0[0] = float(getattr(pitch,"vz0",-9999))
+        self._ax[0] = float(getattr(pitch,"ax",-9999))
+        self._ay[0] = float(getattr(pitch,"ay",-9999))
+        self._az[0] = float(getattr(pitch,"az",-9999))
+        self._break_y[0] = float(getattr(pitch,"break_y",-9999))
+        self._break_angle[0] = float(getattr(pitch,"break_angle",-9999))
+        self._break_length[0] = float(getattr(pitch,"break_length",-9999))
+        self._type_confidence[0] = float(getattr(pitch,"type_confidence",-9999))
+        self._zone[0] = int(getattr(pitch,"zone",-9999))
+        self._nasty[0] = int(getattr(pitch,"nasty",-9999))
+        self._spin_dir[0] = float(getattr(pitch,"spin_dir",-9999))
+        self._spin_rate[0] = float(getattr(pitch,"spin_rate",-9999))
+        if pitch.pitch_type not in self._unique_pitch_types:
+            print "NEW PITCH:", pitch.pitch_type
+            self._unique_pitch_types.append(pitch.pitch_type)
+        # self._pitch_type_id[0] = int(self._unique_pitch_types.index(pitch.pitch_type)-1)
+            
+        self._is_last_pitch[0] = 0
+        if self._type=='X' or \
+                (self._type=='B' and self._balls[0]==3) or \
+                (self._strike_type in ['S','C','FB','FT','MB'] and self._strikes[0]==2) or \
+                self._des=="Hit By Pitch":
+            self._is_last_pitch[0] = 1
+
+        # statcast hit data
+        self._hit_x[0] = float(getattr(pitch,"hit_x", -9999))
+        self._hit_y[0] = float(getattr(pitch,"hit_y", -9999))
+        self._hit_launchAngle[0] = float(getattr(pitch,"hit_launchAngle", -9999))
+        self._hit_launchSpeed[0] = float(getattr(pitch,"hit_launchSpeed", -9999))
+        self._hit_totalDistance[0] = float(getattr(pitch,"hit_totalDistance", -9999))
+        self._hit_location[0] = int(getattr(pitch,"hit_location", -9999))
+        self._hit_trajectory.replace(0, n, getattr(pitch,"hit_trajectory", "NONE"))
+
+        self._t.Fill()
+
+    def write(self):
+        self._fout.cd()
+        self._t.Write("Pitches",ROOT.TObject.kWriteDelete)
+
+    def __del__(self):
+        self._fout.Close()
